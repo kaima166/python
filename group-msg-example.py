fbbae2c0bdefe4c8d2e138e0b9f3388255fbb96b
kaima166 <45202024+kaima166@users.noreply.github.com>
2018-11-20 21:38:18 +0800

    Add files via upload

---
+++ group-msg-example.py
@@ -0,0 +1,97 @@
+{'MsgId': '1620338495771856925', 'FromUserName':
+'@@fde4ae38b302211ff70a6dfe4b2051375956fc9804d06bbd556ac7c8a844dab3',
+'ToUserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'MsgType': 1, 'Content': '1111', 'Status': 3, 'ImgStatus': 1,
+'CreateTime': 1542720617, 'VoiceLength': 0, 'PlayLength': 0, 'FileName':
+'', 'FileSize': '', 'MediaId': '', 'Url': '', 'AppMsgType': 0,
+'StatusNotifyCode': 0, 'StatusNotifyUserName': '', 'RecommendInfo':
+{'UserName': '', 'NickName': '', 'QQNum': 0, 'Province': '', 'City': '',
+'Content': '', 'Signature': '', 'Alias': '', 'Scene': 0, 'VerifyFlag':
+0, 'AttrStatus': 0, 'Sex': 0, 'Ticket': '', 'OpCode': 0}, 'ForwardFlag':
+0, 'AppInfo': {'AppID': '', 'Type': 0}, 'HasProductId': 0, 'Ticket': '',
+'ImgHeight': 0, 'ImgWidth': 0, 'SubMsgType': 0, 'NewMsgId':
+1620338495771856925, 'OriContent': '', 'EncryFileName': '',
+'ActualNickName': 'Ma Kai', 'IsAt': False, 'ActualUserName':
+'@152663af4a871b03c80ea8f91aac0150', 'User': <Chatroom: {'MemberList':
+<ContactList: [<ChatroomMember: {'MemberList': <ContactList: []>, 'Uin':
+0, 'UserName': '@152663af4a871b03c80ea8f91aac0150', 'NickName': 'Ma
+Kai', 'AttrStatus': 33786303, 'PYInitial': '', 'PYQuanPin': '',
+'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': 'flo'}>, <ChatroomMember: {'MemberList':
+<ContactList: []>, 'Uin': 0, 'UserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'NickName': 'Joy', 'AttrStatus': 33790885, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>, <ChatroomMember: {'MemberList':
+<ContactList: []>, 'Uin': 0, 'UserName':
+'@41ee65ef9a01c37ccaae4365f869345ba639e7a553df94b89ec07a4d9ce69de0',
+'NickName': 'kai', 'AttrStatus': 135973, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>]>, 'Uin': 0, 'UserName':
+'@@fde4ae38b302211ff70a6dfe4b2051375956fc9804d06bbd556ac7c8a844dab3',
+'NickName': '测试群', 'HeadImgUrl':
+'/cgi-bin/mmwebwx-bin/webwxgetheadimg?seq=681480565&username=@@fde4ae38b302211ff70a6dfe4b2051375956fc9804d06bbd556ac7c8a844dab3&skey=',
+'ContactFlag': 2, 'MemberCount': 3, 'RemarkName': '',
+'HideInputBarFlag': 0, 'Sex': 0, 'Signature': '', 'VerifyFlag': 0,
+'OwnerUin': 0, 'PYInitial': 'CSQ', 'PYQuanPin': 'ceshiqun',
+'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'StarFriend': 0,
+'AppAccountFlag': 0, 'Statues': 0, 'AttrStatus': 0, 'Province': '',
+'City': '', 'Alias': '', 'SnsFlag': 0, 'UniFriend': 0, 'DisplayName':
+'', 'ChatRoomId': 0, 'KeyWord': '', 'EncryChatRoomId':
+'@b8dee25680cfe40db8bb2b53d72dbadf', 'IsOwner': 0, 'IsAdmin': None,
+'Self': <ChatroomMember: {'MemberList': <ContactList: []>, 'Uin': 0,
+'UserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'NickName': 'Joy', 'AttrStatus': 33790885, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>}>, 'Type': 'Text', 'Text': '1111'}
+{'MsgId': '7603679815891323189', 'FromUserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'ToUserName':
+'@@fde4ae38b302211ff70a6dfe4b2051375956fc9804d06bbd556ac7c8a844dab3',
+'MsgType': 1, 'Content': '2222', 'Status': 3, 'ImgStatus': 1,
+'CreateTime': 1542720623, 'VoiceLength': 0, 'PlayLength': 0, 'FileName':
+'', 'FileSize': '', 'MediaId': '', 'Url': '', 'AppMsgType': 0,
+'StatusNotifyCode': 0, 'StatusNotifyUserName': '', 'RecommendInfo':
+{'UserName': '', 'NickName': '', 'QQNum': 0, 'Province': '', 'City': '',
+'Content': '', 'Signature': '', 'Alias': '', 'Scene': 0, 'VerifyFlag':
+0, 'AttrStatus': 0, 'Sex': 0, 'Ticket': '', 'OpCode': 0}, 'ForwardFlag':
+0, 'AppInfo': {'AppID': '', 'Type': 0}, 'HasProductId': 0, 'Ticket': '',
+'ImgHeight': 0, 'ImgWidth': 0, 'SubMsgType': 0, 'NewMsgId':
+7603679815891323189, 'OriContent': '', 'EncryFileName': '',
+'ActualNickName': 'Joy', 'IsAt': False, 'ActualUserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'User': <Chatroom: {'MemberList': <ContactList: [<ChatroomMember:
+{'MemberList': <ContactList: []>, 'Uin': 0, 'UserName':
+'@152663af4a871b03c80ea8f91aac0150', 'NickName': 'Ma Kai', 'AttrStatus':
+33786303, 'PYInitial': '', 'PYQuanPin': '', 'RemarkPYInitial': '',
+'RemarkPYQuanPin': '', 'MemberStatus': 0, 'DisplayName': '', 'KeyWord':
+'flo'}>, <ChatroomMember: {'MemberList': <ContactList: []>, 'Uin': 0,
+'UserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'NickName': 'Joy', 'AttrStatus': 33790885, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>, <ChatroomMember: {'MemberList':
+<ContactList: []>, 'Uin': 0, 'UserName':
+'@41ee65ef9a01c37ccaae4365f869345ba639e7a553df94b89ec07a4d9ce69de0',
+'NickName': 'kai', 'AttrStatus': 135973, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>]>, 'Uin': 0, 'UserName':
+'@@fde4ae38b302211ff70a6dfe4b2051375956fc9804d06bbd556ac7c8a844dab3',
+'NickName': '测试群', 'HeadImgUrl':
+'/cgi-bin/mmwebwx-bin/webwxgetheadimg?seq=681480565&username=@@fde4ae38b302211ff70a6dfe4b2051375956fc9804d06bbd556ac7c8a844dab3&skey=',
+'ContactFlag': 2, 'MemberCount': 3, 'RemarkName': '',
+'HideInputBarFlag': 0, 'Sex': 0, 'Signature': '', 'VerifyFlag': 0,
+'OwnerUin': 0, 'PYInitial': 'CSQ', 'PYQuanPin': 'ceshiqun',
+'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'StarFriend': 0,
+'AppAccountFlag': 0, 'Statues': 0, 'AttrStatus': 0, 'Province': '',
+'City': '', 'Alias': '', 'SnsFlag': 0, 'UniFriend': 0, 'DisplayName':
+'', 'ChatRoomId': 0, 'KeyWord': '', 'EncryChatRoomId':
+'@b8dee25680cfe40db8bb2b53d72dbadf', 'IsOwner': 0, 'IsAdmin': None,
+'Self': <ChatroomMember: {'MemberList': <ContactList: []>, 'Uin': 0,
+'UserName':
+'@ec2fbdfa2aaa7fc1ee6a1e8bb55d2582bf258e4475aa6aeae1890a0b35944f25',
+'NickName': 'Joy', 'AttrStatus': 33790885, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>}>, 'Type': 'Text', 'Text': '2222'}
