5d5c99291b5b32c12489271d9b6f98d2f1e4138c
kaima166 <45202024+kaima166@users.noreply.github.com>
2018-11-21 13:45:29 +0800

    Add files via upload

---
+++ Group_msg_example.py
@@ -0,0 +1,72 @@
+{'MsgId': '6455496248530036618',
+
+ 'FromUserName':
+'@@3328dff7753695bac6dc73f2754c2c9404fa3adad438a9184ecc1c2e308f2d6a',
+
+'ToUserName':
+'@d982f2ac568569fd48e9c61a71b64f548da98464c71223363e6369043b1a2a57',
+
+'MsgType': 1, 'Content': 'Hello ', 'Status': 3, 'ImgStatus': 1,
+'CreateTime': 1542611520, 'VoiceLength': 0, 'PlayLength': 0, 'FileName':
+'', 'FileSize': '', 'MediaId': '', 'Url': '', 'AppMsgType': 0,
+'StatusNotifyCode': 0, 'StatusNotifyUserName': '', 'RecommendInfo':
+{'UserName': '', 'NickName': '', 'QQNum': 0, 'Province': '', 'City': '',
+'Content': '', 'Signature': '', 'Alias': '', 'Scene': 0, 'VerifyFlag':
+0, 'AttrStatus': 0, 'Sex': 0, 'Ticket': '', 'OpCode': 0}, 'ForwardFlag':
+0, 'AppInfo': {'AppID': '', 'Type': 0}, 'HasProductId': 0, 'Ticket': '',
+'ImgHeight': 0, 'ImgWidth': 0, 'SubMsgType': 0, 'NewMsgId':
+6455496248530036618, 'OriContent': '', 'EncryFileName': '',
+ 
+'ActualNickName': 'Ma Kai', 'IsAt': False, 'ActualUserName':
+'@0ab2e505032df6c2235421c7e00dfffa',
+
+ 'User': <Chatroom: {'MemberList':
+<ContactList: [<ChatroomMember: {'MemberList': <ContactList: []>, 'Uin':
+0, 'UserName':
+'@e0779338b3c2a8131e4ce1a10608d05a3285117ffbee13314e3ac6618263ee70',
+'NickName': 'Jennyzhao', 'AttrStatus': 33555749, 'PYInitial': '',
+'PYQuanPin': '', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '',
+'MemberStatus': 0, 'DisplayName': '', 'KeyWord': ''}>,
+
+               <ChatroomMember:
+{'MemberList': <ContactList: []>, 'Uin': 0, 'UserName':
+'@0ab2e505032df6c2235421c7e00dfffa', 'NickName': 'Ma Kai', 'AttrStatus':
+33786303, 'PYInitial': '', 'PYQuanPin': '', 'RemarkPYInitial': '',
+'RemarkPYQuanPin': '', 'MemberStatus': 0, 'DisplayName': '', 'KeyWord':
+'flo'}>, <
+               ChatroomMember: {'MemberList': <ContactList: []>, 'Uin': 0,
+'UserName':
+'@af0c88297c03db1951f422dea81f77bd52c73261273aec593f758b7e26bc291a',
+'NickName': '金豆', 'AttrStatus': 33657637, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>,
+
+               <ChatroomMember: {'MemberList':
+<ContactList: []>, 'Uin': 0, 'UserName':
+'@d982f2ac568569fd48e9c61a71b64f548da98464c71223363e6369043b1a2a57',
+'NickName': 'Joy', 'AttrStatus': 33790885, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>]>,
+
+                     'Uin': 0, 'UserName':
+'@@3328dff7753695bac6dc73f2754c2c9404fa3adad438a9184ecc1c2e308f2d6a',
+'NickName': '爸爸妈妈', 'HeadImgUrl':
+'/cgi-bin/mmwebwx-bin/webwxgetheadimg?seq=681476932&username=@@3328dff7753695bac6dc73f2754c2c9404fa3adad438a9184ecc1c2e308f2d6a&skey=',
+'ContactFlag': 2, 'MemberCount': 4, 'RemarkName': '',
+'HideInputBarFlag': 0, 'Sex': 0, 'Signature': '', 'VerifyFlag': 0,
+'OwnerUin': 0, 'PYInitial': 'BBMM', 'PYQuanPin': 'babamama',
+'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'StarFriend': 0,
+'AppAccountFlag': 0, 'Statues': 1, 'AttrStatus': 0, 'Province': '',
+'City': '', 'Alias': '', 'SnsFlag': 0, 'UniFriend': 0, 'DisplayName':
+'', 'ChatRoomId': 0, 'KeyWord': '', 'EncryChatRoomId':
+'@f041317f57b1029750c7f3b28bff1575', 'IsOwner': 0, 'IsAdmin': None,
+'Self': <ChatroomMember: {'MemberList': <ContactList: []>, 'Uin': 0,
+'UserName':
+'@d982f2ac568569fd48e9c61a71b64f548da98464c71223363e6369043b1a2a57',
+'NickName': 'Joy', 'AttrStatus': 33790885, 'PYInitial': '', 'PYQuanPin':
+'', 'RemarkPYInitial': '', 'RemarkPYQuanPin': '', 'MemberStatus': 0,
+'DisplayName': '', 'KeyWord': ''}>}>, 'Type': 'Text', 'Text': 'Hello '}
+{'6455496248530036618': {'msg_from': 'Ma Kai', 'msg_time': 1542611520,
+'msg_time_rec': '2018-11-19 15:12:01', 'msg_type': 'Text',
+'msg_content': 'Hello ', 'msg_share_url': None, 'group_name':
+'爸爸妈妈(4)'}}
---
+++ single_msg_example.py
@@ -0,0 +1,33 @@
+{'MsgId': '7923837799497939290',
+
+ 'FromUserName':
+'@4aa48a7a703cb9b5616beb3683a0c789',
+
+ 'ToUserName':
+'@5c39728a751b1aa488b0363c1b8ee994b8b496f599e763e09542b561eaef48a4',
+'MsgType': 1, 'Content': 'test', 'Status': 3, 'ImgStatus': 1,
+'CreateTime': 1542594182, 'VoiceLength': 0, 'PlayLength': 0, 'FileName':
+'', 'FileSize': '', 'MediaId': '', 'Url': '', 'AppMsgType': 0,
+'StatusNotifyCode': 0, 'StatusNotifyUserName': '', 'RecommendInfo':
+
+{'UserName': '', 'NickName': '', 'QQNum': 0, 'Province': '', 'City': '',
+'Content': '', 'Signature': '', 'Alias': '', 'Scene': 0, 'VerifyFlag':
+0, 'AttrStatus': 0, 'Sex': 0, 'Ticket': '', 'OpCode': 0}, 'ForwardFlag':
+0, 'AppInfo': {'AppID': '', 'Type': 0}, 'HasProductId': 0, 'Ticket': '',
+'ImgHeight': 0, 'ImgWidth': 0, 'SubMsgType': 0, 'NewMsgId':
+7923837799497939290, 'OriContent': '', 'EncryFileName': '',
+
+ 'User':
+<User: {'MemberList': <ContactList: []>, 'Uin': 0, 'UserName':
+'@4aa48a7a703cb9b5616beb3683a0c789', 'NickName': 'Ma Kai', 'HeadImgUrl':
+'/cgi-bin/mmwebwx-bin/webwxgeticon?seq=675139899&username=@4aa48a7a703cb9b5616beb3683a0c789&skey=@crypt_b21d2b12_8eadc13b20e2e4d14084828b5cbd8a31',
+'ContactFlag': 8259, 'MemberCount': 0, 'RemarkName': 'Ma Kai',
+'HideInputBarFlag': 0, 'Sex': 1, 'Signature': '「生活是一次旅行，没有输赢，只有看了多少风景」
+', 'VerifyFlag': 0, 'OwnerUin': 0, 'PYInitial': 'MAKAI', 'PYQuanPin':
+'MaKai', 'RemarkPYInitial': 'MAKAI', 'RemarkPYQuanPin': 'MaKai',
+'StarFriend': 1, 'AppAccountFlag': 0, 'Statues': 0, 'AttrStatus':
+33786303, 'Province': '江苏', 'City': '南京', 'Alias': '', 'SnsFlag': 49,
+'UniFriend': 0, 'DisplayName': '', 'ChatRoomId': 0, 'KeyWord': 'flo',
+'EncryChatRoomId': '', 'IsOwner': 0}>,
+
+ 'Type': 'Text', 'Text': 'test'}
